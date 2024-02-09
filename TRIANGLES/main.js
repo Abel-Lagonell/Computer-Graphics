915f8ec98a7b6437690cea00a219331238899171
Abel-Lagonell <alagonell1730@floridapoly.edu>
2024-02-09 12:11:19 -0500

    Fixed minor thing

--- TRIANGLES/main.js
+++ TRIANGLES/main.js
@@ -25,7 +25,7 @@ class Main {
             0.5, 1, 0, 1, 0,
             0.5, 0, 0, 1, 1,
             0, 	-1, 0, 0, 1,
-            -0.5, 1, 1, 0, 1,
+            -0.5, 0, 1, 0, 1,
         ];
 
         let temp = new Shape(this.gl, this.positions, 6);
