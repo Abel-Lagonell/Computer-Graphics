25da86e7d95a7946cff8fe2234160ecefea80873
AbelLagonell <alagonell@outlook.com>
2025-11-11 12:48:50 -0500

    GLTF parsing so far

---
+++ WebGpu/GLTF Workflow/Site/Main.js
@@ -0,0 +1,113 @@
+﻿const Type = Object.freeze({
+    "SCALAR": 1,
+    "VEC2": 2,
+    "VEC3": 3,
+    "VEC4": 4,
+    "MAT2": 4,
+    "MAT3": 9,
+    "MAT4": 16,
+})
+
+const ComponentType = Object.freeze({
+    5120: Int8Array,
+    5121: Uint8Array,
+    5122: Int16Array,
+    5123: Uint16Array,
+    5125: Int32Array,
+    5126: Uint32Array,
+})
+
+class Main {
+    /** @type */
+    arrayBuffer;
+
+    constructor() {
+        this.SlowStart()
+    }
+
+    async SlowStart() {
+        const text_gltf = await this.LoadFile("../GLTF/TextureSample.gltf");
+        /** @type {GLTF} */
+        this.gltf = JSON.parse(text_gltf);
+        console.log(this.gltf);
+        const text_bin = await this.LoadBin("../GLTF/TextureSample.bin");
+        while (!this.arrayBuffer) {
+            await this.Sleep(200);
+        }
+        document.getElementById("log").innerHTML = text_gltf;
+
+        let vec = await this.ArrayGetter(this.arrayBuffer, this.gltf.bufferViews[1].byteLength, this.gltf.bufferViews[1].byteOffset, Type.VEC3, this.gltf.accessors[1].componentType)
+
+        this.ParseGLTF(this.gltf)
+    }
+
+    /**
+     * @param gltf : GLTF
+     * @constructor
+     */
+    ParseGLTF(gltf) {
+        /** @type GLTFScene */
+        var defaultScene = gltf.scenes[gltf.scene]
+
+        for (let node in defaultScene.nodes) {
+            for (let mesh in node) {
+                for (let primitive of gltf.meshes[mesh].primitives) {
+                    for (const key in primitive.attributes){
+                        console.log(gltf.accessors[primitive.attributes[key]]);
+                    } 
+                } 
+            }
+        }
+    }
+
+    Sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async LoadFile(url) {
+        const response = await fetch(url);
+        return await response.text();
+    }
+
+    async LoadBin(url) {
+        const req = new XMLHttpRequest();
+        req.open("GET", url, true);
+        req.responseType = "arraybuffer";
+
+        req.onload = (e) => {
+            const arrayBuffer = req.response;
+            if (arrayBuffer) {
+                this.arrayBuffer = arrayBuffer;
+                return this.arrayBuffer;
+            }
+        }
+        req.send(null);
+    }
+
+    /** @param accessor : GLTFAccessor */
+    async AccessorGetter(accessor){
+        
+    }
+    
+    async ArrayGetter(buffer, bufferLength, bufferOffset, type, componentType) {
+        console.log(bufferLength, bufferOffset);
+        bufferLength = bufferLength / 4;
+        bufferOffset = bufferOffset / 4;
+        var newBuffer = new Float32Array(buffer);
+        let vectorArray = [];
+        let temp = [];
+        for (let i = bufferOffset; i < bufferOffset + bufferLength; i++) {
+            temp[i % Type[type]] = newBuffer[i];
+            if (temp.length === Type[type]) {
+                vectorArray.push(temp);
+                temp = []
+            }
+        }
+        return vectorArray;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Running Main");
+    let m = new Main();
+})
---
+++ WebGpu/GLTF Workflow/Site/types.js
@@ -0,0 +1,139 @@
+﻿/**
+ * @typedef {Object} GLTF
+ * @property {string} [asset.version] - The glTF version (e.g. "2.0").
+ * @property {string} [asset.generator] - The software that generated this glTF.
+ * @property {GLTFScene[]} [scenes] - A list of scenes.
+ * @property {number} [scene] - The index of the default scene.
+ * @property {GLTFNode[]} [nodes] - A list of node objects.
+ * @property {GLTFMesh[]} [meshes] - A list of mesh objects.
+ * @property {GLTFAccessor[]} [accessors] - A list of accessors.
+ * @property {GLTFBufferView[]} [bufferViews] - A list of buffer views.
+ * @property {GLTFBuffer[]} [buffers] - A list of buffers.
+ * @property {GLTFMaterial[]} [materials] - A list of materials.
+ * @property {GLTFTexture[]} [textures] - A list of textures.
+ * @property {GLTFImage[]} [images] - A list of images.
+ * @property {GLTFSampler[]} [samplers] - A list of samplers.
+ * @property {GLTFAnimation[]} [animations] - A list of animations.
+ * @property {GLTFCamera[]} [cameras] - A list of cameras.
+ */
+
+/**
+ * @typedef {Object} GLTFScene
+ * @property {string} [name]
+ * @property {number[]} [nodes]
+ */
+
+/**
+ * @typedef {Object} GLTFNode
+ * @property {string} [name]
+ * @property {number} [mesh]
+ * @property {number[]} [children]
+ * @property {number[]} [translation]
+ * @property {number[]} [rotation]
+ * @property {number[]} [scale]
+ * @property {number[]} [matrix]
+ */
+
+/**
+ * @typedef {Object} GLTFMesh
+ * @property {string} [name]
+ * @property {GLTFPrimitive[]} [primitives]
+ */
+
+/**
+ * @typedef {Object} GLTFPrimitive
+ * @property {Object.<string, number>} attributes - Mapping of attribute semantic (e.g. "POSITION") to accessor index.
+ * @property {number} [indices] - The accessor index of indices.
+ * @property {number} [material] - The material index.
+ * @property {number} [mode] - The topology type (e.g. 4 for TRIANGLES).
+ */
+
+/**
+ * @typedef {Object} GLTFAccessor
+ * @property {number} bufferView
+ * @property {number} byteOffset
+ * @property {number} componentType
+ * @property {boolean} [normalized]
+ * @property {number} count
+ * @property {string} type
+ * @property {number[]} [max]
+ * @property {number[]} [min]
+ */
+
+/**
+ * @typedef {Object} GLTFBufferView
+ * @property {number} buffer
+ * @property {number} [byteOffset]
+ * @property {number} byteLength
+ * @property {number} [byteStride]
+ * @property {number} [target]
+ */
+
+/**
+ * @typedef {Object} GLTFBuffer
+ * @property {string} uri
+ * @property {number} byteLength
+ */
+
+/**
+ * @typedef {Object} GLTFMaterial
+ * @property {string} [name]
+ * @property {Object} [pbrMetallicRoughness]
+ * @property {Object} [normalTexture]
+ * @property {Object} [occlusionTexture]
+ * @property {Object} [emissiveTexture]
+ * @property {number[]} [emissiveFactor]
+ * @property {string} [alphaMode]
+ * @property {number} [alphaCutoff]
+ * @property {boolean} [doubleSided]
+ */
+
+/**
+ * @typedef {Object} GLTFTexture
+ * @property {number} [sampler]
+ * @property {number} [source]
+ */
+
+/**
+ * @typedef {Object} GLTFImage
+ * @property {string} [uri]
+ * @property {string} [mimeType]
+ * @property {number} [bufferView]
+ */
+
+/**
+ * @typedef {Object} GLTFSampler
+ * @property {number} [magFilter]
+ * @property {number} [minFilter]
+ * @property {number} [wrapS]
+ * @property {number} [wrapT]
+ */
+
+/**
+ * @typedef {Object} GLTFAnimation
+ * @property {string} [name]
+ * @property {GLTFAnimationSampler[]} [samplers]
+ * @property {GLTFAnimationChannel[]} [channels]
+ */
+
+/**
+ * @typedef {Object} GLTFAnimationSampler
+ * @property {number} input
+ * @property {string} interpolation
+ * @property {number} output
+ */
+
+/**
+ * @typedef {Object} GLTFAnimationChannel
+ * @property {number} sampler
+ * @property {Object} target
+ * @property {number} target.node
+ * @property {string} target.path
+ */
+
+/**
+ * @typedef {Object} GLTFCamera
+ * @property {string} type
+ * @property {Object} [perspective]
+ * @property {Object} [orthographic]
+ */
