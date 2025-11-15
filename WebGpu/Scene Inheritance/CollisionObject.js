d035899f1975f44965cbc45cb34381e3638aa2e8
AbelLagonell <alagonell@outlook.com>
2025-11-15 14:27:03 -0500

    passing collision objects into computer

---
+++ WebGpu/Scene Inheritance/CollisionObject.js
@@ -0,0 +1,21 @@
+﻿import {Transform} from "./Transform";
+import {Vector3} from "./Vector3";
+import {Shape} from "./Shape/Shape.js";
+
+class CollisionObject extends Transform {
+    constructor(options = {}) {
+        const {
+            name = "CollisionObject",
+            position = Vector3.Zero.copy(),
+            rotation = Vector3.Zero.copy(),
+            scale = Vector3.One.copy(),
+            boundSize = new Shape,
+        } = options;
+        
+        super(name,{...options});
+    }
+
+    OnTriggerEnter(){}
+    
+    OnCollisionEnter(){}
+}
---
+++ WebGpu/Scene Inheritance/Shape/Shape.js
@@ -0,0 +1,10 @@
+﻿import {Vector3} from "../Vector3.js";
+
+export class Shape {
+    constructor(options = {}) {
+        const {
+            position= Vector3.Zero.copy(),
+            rotation= Vector3.Zero.copy(),
+        } = options;
+    }
+}
